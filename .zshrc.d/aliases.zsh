b6db13a87deaba45b164c9dfbc2344553a65ec75
mr_pom <julien.pommier@quizzbox.com>
2024-10-23 11:29:28 +0200

    🔧 Creating basic default config

--- .zshrc.d/aliases.zsh
+++ .zshrc.d/aliases.zsh
@@ -3,38 +3,62 @@
 # .aliases - Set whatever shell aliases you want.
 #
 
-# single character aliases - be sparing!
+# Single character aliases - be sparing!
 alias _=sudo
 alias l=ls
 alias g=git
 
-# mask built-ins with better defaults
+# Mask built-ins with better defaults.
 alias vi=vim
+alias ls='lsd'
 
-# more ways to ls
+# Colorize grep.
+alias grep='grep --color=auto -i'
+
+# More ways to ls.
 alias ll='ls -lh'
 alias la='ls -lAh'
 alias ldot='ls -ld .*'
 
-# fix common typos
+# Fix common typos.
 alias quit='exit'
 alias cd..='cd ..'
 
-# tar
+# Navigation enhancement.
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+alias .......='cd ../../../../../..'
+
+# Awk shortcuts.
+alias ap1="awk '{print \$1}'"
+alias ap2="awk '{print \$2}'"
+alias ap3="awk '{print \$3}'"
+alias ap4="awk '{print \$4}'"
+alias ap5="awk '{print \$5}'"
+alias ap6="awk '{print \$6}'"
+alias ap7="awk '{print \$7}'"
+alias ap8="awk '{print \$8}'"
+alias ap9="awk '{print \$9}'"
+alias ap10="awk '{print \$10}'"
+
+# Tar.
 alias tarls="tar -tvf"
 alias untar="tar -xf"
 
-# find
+# Find.
 alias fd='find . -type d -name'
 alias ff='find . -type f -name'
 
-# url encode/decode
+# Url encode/decode.
 alias urldecode='python3 -c "import sys, urllib.parse as ul; \
     print(ul.unquote_plus(sys.argv[1]))"'
 alias urlencode='python3 -c "import sys, urllib.parse as ul; \
     print (ul.quote_plus(sys.argv[1]))"'
 
-# misc
+# Misc.
 alias please=sudo
 alias zshrc='${EDITOR:-vim} "${ZDOTDIR:-$HOME}"/.zshrc'
 alias zbench='for i in {1..10}; do /usr/bin/time zsh -lic exit; done'
---
+++ .zshrc.d/fzf.zsh
@@ -0,0 +1,17 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+source "/opt/homebrew/opt/fzf/shell/completion.zsh"
+
+# Key bindings
+# ------------
+source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
+
+# Dracula theme
+# -------------
+export FZF_DEFAULT_OPTS='--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4'
